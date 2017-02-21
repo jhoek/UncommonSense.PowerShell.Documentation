b0c173f20171d5c678b39a9bab84ad5bbc546578
Jan Hoek <github@uncommonsense.nl>
2017-02-21 20:03:28 +0100

    Initial commit.

---
+++ Get-HelpAsMarkDown.ps1
@@ -0,0 +1,178 @@
+﻿<#
+.Synopsis
+Formats cmdlet help as MarkDown
+.Example
+Get-Command -Module IDYN.NAV.Automation | Sort-Object -Property Verb | Get-HelpAsMarkDown -Title IDYN.NAV.Automation -Description 'PowerShell cmdlets for IDYN developers.' | clip
+Documents module IDYN.NAV.Automation, sorts the functions by verb name, adds a module title and description and copies the resulting text to the clipboard
+#>
+function Get-HelpAsMarkDown
+{
+    [CmdletBinding()]
+    Param
+    (
+        # The command or commands to include in the MarkDown file
+        [Parameter(Mandatory,ValueFromPipeLine)]
+        $Commands,
+
+        # The title for the MarkDown file
+        [string]
+        $Title,
+
+        # A description describing this group of commands, e.g. a short module description
+        [string]
+        $Description,
+
+        # The path of the preface file. This file will be included in the output before the actual command help
+        [ValidateScript( { Test-Path $_ } )]
+        [string]
+        $PrefacePath,
+
+        # The path of the postface file. This file will be included in the output after the actual command help
+        [ValidateScript( { Test-Path $_ } )]
+        [string]
+        $PostfacePath
+    )
+
+    Begin
+    {
+        Add-Type -AssemblyName System.Web
+
+        $CachedCommands = @()
+        $Activity = 'Formatting help info as MarkDown'
+    }
+    Process
+    {
+        foreach($Command in $Commands)
+        {
+            $CachedCommands += $Command
+        }
+    }
+    End
+    {
+        $NoOfCommands = $CachedCommands.Length
+        $IndexRequired = $CachedCommands.Length -gt 1
+
+        if ($Title)
+        {
+            Write-Output "# $Title"
+            Write-Output ''
+        }
+        
+        if ($Description)
+        {
+            Write-Output $Description
+            Write-Output ''
+        }
+
+        if ($PrefacePath)
+        {
+            Write-Output (Get-Content -Path $PrefacePath)
+            Write-Output ''
+        }
+
+        if ($IndexRequired)
+        {
+            Write-Output '## Index'
+            Write-Output ''
+            
+            Write-Output '| Command | Synopsis |'
+            Write-Output '| ------- | -------- |'
+
+            $CurrentCommand = 0
+            foreach($Command in $CachedCommands)
+            {
+                $CurrentCommand++
+                Write-Progress -Activity $Activity -CurrentOperation 'Creating index' -PercentComplete ($CurrentCommand / $NoOfCommands * 100)
+
+                $HelpInfo = Get-Help $Command -Full
+                $LeftColumn = "[$($Command.Name)](#$($Command.Name))"
+                $RightColumn = "$(($HelpInfo.Synopsis | Out-String -Width 1200).Trim())" -replace '\n', ' '
+
+                Write-Output "| $LeftColumn | $RightColumn |"
+            }
+            
+            Write-Output ''
+        }
+
+        $CurrentCommand = 0
+        foreach($Command in $CachedCommands)
+        {
+            $CurrentCommand++
+            Write-Progress -Activity $Activity -CurrentOperation $Command.Name -PercentComplete ($CurrentCommand / $NoOfCommands * 100)
+
+            $HelpInfo = Get-Help $Command -Full
+            
+            # Name
+            Write-Output "<a name=`"$($Command.Name)`"></a>"
+            Write-Output "## $($HelpInfo.Name)"
+
+            # Synopsis
+            Write-Output '### Synopsis'
+            Write-Output ($HelpInfo.Synopsis | Out-String -Width 1200).Trim()
+
+            # Description
+            if ('Description' -in $HelpInfo.PSObject.Properties.Name)
+            {
+                Write-Output '### Description'
+                Write-Output ($HelpInfo.Description | Out-String -Width 1200).Trim()
+                Write-Output ''
+            }
+
+            # Syntax
+            Write-Output '### Syntax'
+            Write-Output ``````powershell
+            Write-Output ($Command | Get-Command -Syntax).Trim()
+            Write-Output ``````
+
+            # Parameters
+            Write-Output '### Parameters'
+            foreach($Parameter in $HelpInfo.Parameters.Parameter)
+            {
+                $ParameterText = $Parameter | Out-String -Width 1200
+                #Write-Debug $ParameterText
+                $ParameterText = $ParameterText -replace '^\s*-', '#### '
+                $ParameterText = $ParameterText.Trim()
+                $ParameterText = [System.Web.HttpUtility]::HtmlEncode($ParameterText)
+
+                Write-Output $($ParameterText)
+            }
+
+            # Examples
+            if ($HelpInfo.Examples) 
+            # if ('Examples' -in $HelpInfo.PSObject.Properties.Name)
+            {
+                Write-Output '### Examples'
+                foreach($Example in $HelpInfo.Examples.Example)
+                {
+                    $ExampleTitle = $Example.Title
+                    $ExampleTitle = $ExampleTitle -replace '-', ''
+                    $ExampleTitle = $ExampleTitle -replace '^\s*', '#### '
+                    $ExampleTitle = $ExampleTitle -replace 'EXAMPLE', 'Example'
+
+                    Write-Output $ExampleTitle
+                    Write-Output ```````powershell
+               
+                    if($Example.Code)
+                    {
+                        Write-Output $Example.Code
+                        Write-output ''
+                    }
+
+                    Write-Output ```````
+
+                    if ($Example.Remarks)
+                    {
+                        Write-Output "$(($Example.Remarks | Out-String -Width 1200).Trim())"
+                    }
+                }
+            }
+        }
+
+        if ($PostfacePath)
+        {
+            Write-Output (Get-Content -Path $PostfacePath)
+        }
+
+        Write-Output "<div style='font-size:small; color: #ccc'>Generated $(Get-Date -Format 'dd-MM-yyyy HH:mm')</div>"
+    }
+}
---
+++ README.md
@@ -0,0 +1,57 @@
+﻿<a name="Get-HelpAsMarkDown"></a>
+## Get-HelpAsMarkDown
+### Synopsis
+Formats PowerShell cmdlet help as MarkDown
+### Syntax
+```powershell
+Get-HelpAsMarkDown [-Commands] <Object> [[-Title] <string>] [[-Description] <string>] [[-PrefacePath] <string>] [[-PostfacePath] <string>] [<CommonParameters>]
+```
+### Parameters
+#### Commands &lt;Object&gt;
+    The command or commands to include in the MarkDown file
+    
+    Required?                    true
+    Position?                    1
+    Default value                
+    Accept pipeline input?       true (ByValue)
+    Accept wildcard characters?  false
+#### Title &lt;String&gt;
+    The title for the MarkDown file
+    
+    Required?                    false
+    Position?                    2
+    Default value                
+    Accept pipeline input?       false
+    Accept wildcard characters?  false
+#### Description &lt;String&gt;
+    A description describing this group of commands, e.g. a short module description
+    
+    Required?                    false
+    Position?                    3
+    Default value                
+    Accept pipeline input?       false
+    Accept wildcard characters?  false
+#### PrefacePath &lt;String&gt;
+    The path of the preface file. This file will be included in the output before the actual command help
+    
+    Required?                    false
+    Position?                    4
+    Default value                
+    Accept pipeline input?       false
+    Accept wildcard characters?  false
+#### PostfacePath &lt;String&gt;
+    The path of the postface file. This file will be included in the output after the actual command help
+    
+    Required?                    false
+    Position?                    5
+    Default value                
+    Accept pipeline input?       false
+    Accept wildcard characters?  false
+### Examples
+#### Example 1 
+```powershell
+Get-Command -Module IDYN.NAV.Automation | Sort-Object -Property Verb | Get-HelpAsMarkDown -Title IDYN.NAV.Automation -Description 'PowerShell cmdlets for IDYN developers.' | clip
+
+```
+Documents module IDYN.NAV.Automation, sorts the functions by verb name, adds a module title and description and copies the resulting text to the clipboard
+<div style='font-size:small; color: #ccc'>Generated 21-02-2017 19:54</div>
