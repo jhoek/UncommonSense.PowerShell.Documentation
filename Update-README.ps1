6bf09d9441afdfbd78b23fb72f1a7dc37ede2cad
Jan Hoek <github@uncommonsense.nl>
2017-12-02 10:56:39 +0100

    Updated script for generating README.md

--- Update-README.ps1
+++ Update-README.ps1
@@ -1,3 +1,9 @@
-﻿. (Join-Path -Path $PSScriptRoot -ChildPath .\Get-HelpAsMarkDown.ps1)
+﻿Import-Module Get-HelpAsMarkDown -Force
 
-Get-Command Get-HelpAsMarkDown | Get-HelpAsMarkDown | Out-File .\README.md -Encoding utf8
+Get-Command -Module Get-HelpAsMarkDown |
+    Sort-Object Noun, Verb |
+    Get-HelpAsMarkDown `
+    -Title 'Get-HelpAsMarkDown' `
+    -Description 'PowerShell module for converting PowerShell help information to MarkDown' `
+    -PrefacePath ./PREFACE.md | 
+    Out-File .\README.md -Encoding utf8
