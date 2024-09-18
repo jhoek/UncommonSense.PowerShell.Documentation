c609cbc8f2c4ab75261c7a1deda8b9c98dc3af23
Jan Hoek <github@uncommonsense.nl>
2024-09-18 12:23:33 +0200

    Added dotnet project

--- ExportModuleDocumentationCmdlet.cs
+++ ExportModuleDocumentationCmdlet.cs
@@ -1,15 +1,32 @@
 ﻿using System.Management.Automation;
+using System.Security.Cryptography.X509Certificates;
 
 namespace UncommonSense.PowerShell.Documentation;
 
-[Cmdlet(VerbsData.Export, Nouns.ModuleDocumentation)]
+[Cmdlet(VerbsData.Export, Nouns.ModuleDocumentation, DefaultParameterSetName = ParameterSets.FromModuleToText)]
 [Alias("Convert-HelpToMarkDown")]
 public class ExportModuleDocumentationCmdlet : PSCmdlet
 {
+    public static class ParameterSets
+    {
+        public const string FromModuleToFile = nameof(FromModuleToFile);
+        public const string FromModuleToText = nameof(FromModuleToText);
+        public const string FromMethodsToFile = nameof(FromMethodsToFile);
+        public const string FromMethodsToText = nameof(FromMethodsToText);
+    }
+
+    [Parameter(Mandatory = true, ParameterSetName = ParameterSets.FromModuleToFile, ValueFromPipeline = true)]
+    [Parameter(Mandatory = true, ParameterSetName = ParameterSets.FromModuleToText, ValueFromPipeline = true)]
+    public PSModuleInfo[] Module { get; set; }
 
 
+
+
+
+
+    // FIXME: Title and Description parameters when passing in methods
     // FIXME: Preface/postface from file or literal
     // FIXME: List dependencies, installation instructions
     // FIXME: To file or as text
-    // FIXME: From moduleinfo or from methodinfo[]
+    // FIXME: From moduleinfo or from methodinfo[]; output resp. to directory or path
 }
--- test.ps1
+++ test.ps1
@@ -1,4 +1,1 @@
-Import-Module $PSScriptRoot/output/UncommonSense.PowerShell.Documentation/UncommonSense.PowerShell.Documentation.psd1
-Get-Command Export-ModuleDocumentation
-
 Get-Module UncommonSense.Timesheet | Export-ModuleDocumentation
