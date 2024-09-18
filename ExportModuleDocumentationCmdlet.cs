f2f3306d1ebd538e330484d49783273e66635797
Jan Hoek <github@uncommonsense.nl>
2024-09-18 12:41:10 +0200

    Added dotnet project

--- ExportModuleDocumentationCmdlet.cs
+++ ExportModuleDocumentationCmdlet.cs
@@ -19,14 +19,52 @@ public class ExportModuleDocumentationCmdlet : PSCmdlet
     [Parameter(Mandatory = true, ParameterSetName = ParameterSets.FromModuleToText, ValueFromPipeline = true)]
     public PSModuleInfo[] Module { get; set; }
 
+    [Parameter(Mandatory = true, ParameterSetName = ParameterSets.FromMethodsToFile, ValueFromPipeline = true)]
+    [Parameter(Mandatory = true, ParameterSetName = ParameterSets.FromMethodsToText, ValueFromPipeline = true)]
+    public CommandInfo[] Command { get; set; }
+
+    [Parameter(Mandatory = true, ParameterSetName = ParameterSets.FromMethodsToFile)]
+    [Parameter(Mandatory = true, ParameterSetName = ParameterSets.FromMethodsToText)]
+    public string Title { get; set; }
+
+    [Parameter(Mandatory = true, ParameterSetName = ParameterSets.FromMethodsToFile)]
+    [Parameter(Mandatory = true, ParameterSetName = ParameterSets.FromMethodsToText)]
+    public string Description { get; set; }
+
+    [Parameter()]
+    public string Preface { get; set; }
+
+    [Parameter()]
+    public string Postface { get; set; }
+
+    [Parameter(Mandatory = true, ParameterSetName = ParameterSets.FromModuleToFile)]
+    [Parameter(Mandatory = true, ParameterSetName = ParameterSets.FromMethodsToFile)]
+    public string Path { get; set; }
+
+    [Parameter(Mandatory = true, ParameterSetName = ParameterSets.FromModuleToFile)]
+    [Parameter(Mandatory = true, ParameterSetName = ParameterSets.FromMethodsToFile)]
+    public string PassThru { get; set; }
+
+    protected List<PSModuleInfo> ModuleInfoCache = new List<PSModuleInfo>();
+    protected List<CommandInfo> CommandInfoCache = new List<CommandInfo>();
+
+    protected override void ProcessRecord()
+    {
+        ModuleInfoCache.AddRange(Module ?? Array.Empty<PSModuleInfo>());
+        CommandInfoCache.AddRange(Command ?? Array.Empty<CommandInfo>());
+    }
+
+    protected string EffectiveTitle(PSModuleInfo moduleInfo) => Title ?? moduleInfo.Name;
+    protected
+
+    protected override void EndProcessing()
+    {
+
+    }
 
 
 
 
 
-    // FIXME: Title and Description parameters when passing in methods
-    // FIXME: Preface/postface from file or literal
     // FIXME: List dependencies, installation instructions
-    // FIXME: To file or as text
-    // FIXME: From moduleinfo or from methodinfo[]; output resp. to directory or path
 }
