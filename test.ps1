ce54a4e453934ded341bcf396e7bbe49ce890044
Jan Hoek <github@uncommonsense.nl>
2024-10-06 10:05:45 +0200

    Updated test script

--- test.ps1
+++ test.ps1
@@ -1,6 +1,11 @@
-# (Get-Command Export-CommandDocumentation), (Get-Command Export-ModuleDocumentation) | Export-CommandDocumentation -Title 'Title goes here' -Description 'Description goes here'
+(Get-Command Export-CommandDocumentation), (Get-Command Export-ModuleDocumentation)
+| Export-CommandDocumentation -Title 'Title goes here' -Description 'Description goes here'
+| Set-Content -Path ~/Desktop/command.md
 
-$Module = Get-Module UncommonSense.Nrc -ListAvailable | Import-Module
-Export-ModuleDocumentation -Module $Module | Set-Content ~/Desktop/output.md
+code ~/Desktop/command.md
 
-code ~/Desktop/output.md
+$Module = Get-Module UncommonSense.Nrc -ListAvailable | Import-Module -PassThru
+Export-ModuleDocumentation -Module $Module
+| Set-Content -Path ~/Desktop/module.md
+
+code ~/Desktop/module.md
